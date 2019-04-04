fb2e0e2b28def618f50e59cea53fa3b5e6e454d7
Ajirogg <32861990+Ajirogg@users.noreply.github.com>
2019-04-04 15:06:49 -0400

    Création du menu (non lié pour l'instant)

---
+++ app/src/main/java/com/example/naerl/ball/MenuPrinc.java
@@ -0,0 +1,18 @@
+package com.example.naerl.ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuPrinc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_princ);
+    }
+
+    public void LaunchGame(View view) {
+
+    }
+}
